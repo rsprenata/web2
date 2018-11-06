910bb9ab6573054efa7e14034af36f3d4287b3f8
Willian Gomes <williang181@gmail.com>
2018-11-06 21:39:47 -0200

    ConnectionFactory MySQL

--- src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
+++ src/java/com/ufpr/tads/web2/dao/ConnectionFactory.java
@@ -9,20 +9,20 @@ import java.util.logging.Logger;
 public class ConnectionFactory {
 
     public Connection getConnection() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection("jdbc:postgresql://localhost/web2", "postgres", "postgres");
-        } catch (SQLException exception) {
-            System.out.println("Erro ao conectar no banco: " + exception);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
 //        try {
-//            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-//            return DriverManager.getConnection("jdbc:mysql://localhost/banco_web2", "root", "root");
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
+//            Class.forName("org.postgresql.Driver");
+//            return DriverManager.getConnection("jdbc:postgresql://localhost/web2", "postgres", "postgres");
+//        } catch (SQLException exception) {
+//            System.out.println("Erro ao conectar no banco: " + exception);
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
 //        }
+//        return null;
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            return DriverManager.getConnection("jdbc:mysql://localhost/banco_web2", "root", "root");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
